411279d93ae8e0ba1c56f57dcc702a4ba1e549e1
Pedro Potenza <pedrohpotenza@gmail.com>
2022-08-06 02:40:49 -0300

    CommitLint + Husky test 3

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -11,7 +11,7 @@ const Login: React.FC = () => {
 
     return (
     <div>
-        <p>Login</p>
+        <p>Login 2</p>
         <button onClick={handleSheet}> Ir para a ficha </button>
     </div>
   );
